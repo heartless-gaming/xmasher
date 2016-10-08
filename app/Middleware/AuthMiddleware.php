144a0f5e454a7793e029ca40fe2d103157149c84
skullmasher <skullmasher@heartlessgaming.com>
2016-10-08 14:35:11 +0200

    Protecting the change password page access

---
+++ app/Middleware/AuthMiddleware.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Xmasher\Middleware;
+
+class AuthMiddleware extends Middleware
+{
+  public function __invoke($request, $response, $next)
+  {
+    if (!$this->container->auth->check()) {
+      $this->container->flash->addMessage('error', 'Veuillez vous connecter pour accéder au paramètre de votre compte.');
+      return $response->withRedirect($this->container->router->pathFor('auth.signin'));
+    }
+  }
+}
--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use Xmasher\Middleware\AuthMiddleware;
+
 $app->get('/', 'HomeController:index')->setName('home');
 
 $app->get('/auth/signup', 'AuthController:getSignUp')->setName('auth.signup');
@@ -8,8 +10,11 @@ $app->post('/auth/signup', 'AuthController:postSignUp');
 $app->get('/auth/signin', 'AuthController:getSignin')->setName('auth.signin');
 $app->post('/auth/signin', 'AuthController:postSignin');
 
-$app->get('/auth/signout', 'AuthController:getSignOut')->setName('auth.signout');
+$app->group('', function () {
+  $this->get('/auth/signout', 'AuthController:getSignOut')->setName('auth.signout');
 
-$app->get('/account', 'PasswordController:getChangePassword')->setName('account');
-$app->post('/account', 'PasswordController:postChangePassword');
+  $this->get('/account', 'PasswordController:getChangePassword')->setName('account');
+  $this->post('/account', 'PasswordController:postChangePassword');
+})->add(new AuthMiddleware($container));
+
 
